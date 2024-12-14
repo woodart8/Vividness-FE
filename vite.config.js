98fcc3c64c6ad803b5a28378432508e8c54f7c72
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-15 01:01:50 +0900

    Refactor: 프록시 제거

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': 'https://api.vividness.co',  // /api 경로를 localhost:5000으로 프록시
-    },
-  },
+  plugins: [vue()]
 });
