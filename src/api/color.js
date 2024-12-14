4243877f6ad829da6c94715c6ab58770d3dd1a94
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-15 01:19:41 +0900

    Refactor: origin 직접 설정

--- src/api/color.js
+++ src/api/color.js
@@ -1,4 +1,4 @@
-import apiClient from '@/api/axios';
+import apiClient from './axios';
 
 export const getColor = async (hexCode) => {
   try {
--- src/api/palette.js
+++ src/api/palette.js
@@ -1,4 +1,4 @@
-import apiClient from '@/api/axios';
+import apiClient from './axios';
 
 export const createPalette = async (hexCode) => {
   try {
