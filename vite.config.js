83ddbd4c91aae281f7866ede88deb05e408388e8
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-15 00:06:04 +0900

    Build: 환경 변경

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',  // /api 경로를 localhost:5000으로 프록시
+      '/api': 'https://vividness.co',  // /api 경로를 localhost:5000으로 프록시
     },
   },
 });
