84601b868c70b3186a94260200d42818713ad4ef
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-15 20:22:49 +0900

    Feat: 초기 색상 랜덤으로 나오는 기능 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -181,6 +181,10 @@ const closePickerIfOutside = (event) => {
   }
 };
 
+const generateRandomHex = () => {
+  return Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+};
+
 watch(colors, (newColor) => {
   input.value = newColor.hex.slice(1);
   const luminance = calculateLuminance(input.value);
@@ -199,6 +203,9 @@ watch(input, (newValue) => {
 
 onMounted(() => {
   document.addEventListener('mousedown', closePickerIfOutside);
+  const randomHex = generateRandomHex();
+  hexCode.value = randomHex.toUpperCase();
+  colors.value.hex = '#' + randomHex;
 });
 
 onBeforeUnmount(() => {
