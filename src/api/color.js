a25aebf64d408455361e3d9c85e1fa90dd09f289
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-17 20:37:48 +0900

    Feat: 인기 색상 탭 추가

--- src/api/color.js
+++ src/api/color.js
@@ -8,3 +8,21 @@ export const getColor = async (hexCode) => {
     console.error('Get Color Failed: ', error);
   }
 }
+
+export const getPopularColors = async () => {
+  try {
+    const repsponse = await apiClient.get(`/color/popular`)
+    return repsponse.data;
+  } catch (error) {
+    console.error('Get Color Failed: ', error);
+  }
+}
+
+export const addCopyCount = async (hexCode) => {
+  try {
+    const repsponse = await apiClient.put(`/color/hex/${hexCode}`)
+    return repsponse.data;
+  } catch (error) {
+    console.error('Copy Failed: ', error);
+  }
+}
--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,19 +1,42 @@
 <template>
   <header class="common-header">
-    <div class="logo-wrapper">
+    <div class="logo-wrapper" @click="goHome">
       <div class="logo-icon">
         <img src="/src/assets/logo.svg" alt="Logo Icon">
       </div>
       <span class="logo-text">Vividness</span>
     </div>
-    <a href="https://github.com/woodart8" target="_blank" class="github-link">
-      <i class="fa-brands fa-square-github"></i>
-    </a>
+    <nav class="menu-nav">
+      <ul class="menu-list">
+        <li class="menu" @click="goHome">
+          <span class="menu-text">Palette Generator</span>
+          <i class="fa-solid fa-palette"></i>
+        </li>
+        <li class="menu" @click="goPopularHex">
+          <span class="menu-text">Popular Hex</span>
+          <i class="fa-solid fa-fire"></i>
+        </li>
+      </ul>
+      <a href="https://github.com/woodart8" target="_blank" class="github-link">
+        <i class="fa-brands fa-square-github"></i>
+      </a>
+    </nav>
   </header>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push('/');
+}
+
+const goPopularHex = () => {
+  router.push('/popular');
+}
 </script>
 
 <style scoped>
@@ -33,6 +56,7 @@ import { ref } from 'vue';
   align-items: center;
   width: 17rem;
   gap: 1rem;
+  cursor: pointer;
 }
 
 .logo-icon {
@@ -51,6 +75,32 @@ import { ref } from 'vue';
   color: #202020;
 }
 
+.menu-nav {
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+}
+
+.menu-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  gap: 2rem;
+}
+
+.menu {
+  list-style: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  cursor: pointer;
+}
+
+.menu i {
+  display: none;
+  font-size: 3rem;
+}
+
 .github-link{
   display: flex;
   height: 3.6rem;
@@ -62,5 +112,20 @@ import { ref } from 'vue';
   font-size: 3.6rem;
   text-decoration: none;
   color: #202020;
+}
+
+@media screen and (max-width: 600px) {
+  .menu-list {
+    gap: 3rem;
+    margin-right: 1rem;
+  }
+
+  .menu-text {
+    display: none;
+  }
+
+  .menu i {
+    display: inline;
+  }
 }
 </style>
