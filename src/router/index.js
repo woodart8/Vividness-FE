328481481e03fd26cb41393a03493da1682896cc
Kibeom Jeon <kibeom1145@gmail.com>
2024-12-18 20:30:26 +0900

    Feat: 컬러 믹서 메뉴 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import PopularHexView from '../views/PopularHexView.vue';
+import ColorMixerView from '../views/ColorMixerView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/mixer',
+    name: 'mixer',
+    component: ColorMixerView,
   },
   {
     path: '/popular',
